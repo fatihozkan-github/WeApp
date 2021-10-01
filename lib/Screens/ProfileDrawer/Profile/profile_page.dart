b8602f9c82edbe2b4a925bbe937361170b073644
fatihozkan-github <fatih.ozkan.business@gmail.com>
2021-10-01 19:09:39 +0300

    • fixes.

--- lib/Screens/ProfileDrawer/Profile/profile_page.dart
+++ lib/Screens/ProfileDrawer/Profile/profile_page.dart
@@ -70,6 +70,7 @@ class _ProfilePageState extends State<ProfilePage> {
                   SizedBox(height: 10),
                   OrDivider(text: "Rozetler"),
                   _footer(data),
+                  SizedBox(height: 20),
                 ],
               ),
             );
