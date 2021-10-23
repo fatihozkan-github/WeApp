5543ff0b4b8da1c21c7f46cb35daec87da2150a5
Furkan KURT <fkurt97@gmail.com>
2021-10-23 15:13:56 +0300

    Added charge info for admin users.

--- lib/Screens/BottomNavigation/Map/map_view.dart
+++ lib/Screens/BottomNavigation/Map/map_view.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: omit_local_variable_types, prefer_single_quotes
 import 'dart:typed_data';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -13,9 +15,12 @@ import 'dart:ui' as ui;
 
 Future<Uint8List> getBytesFromAsset(String path, int width) async {
   ByteData data = await rootBundle.load(path);
-  ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width);
+  ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+      targetWidth: width);
   ui.FrameInfo fi = await codec.getNextFrame();
-  return (await fi.image.toByteData(format: ui.ImageByteFormat.png)).buffer.asUint8List();
+  return (await fi.image.toByteData(format: ui.ImageByteFormat.png))
+      .buffer
+      .asUint8List();
 }
 
 class MapView extends StatefulWidget {
@@ -28,10 +33,13 @@ class _MapViewState extends State<MapView> {
   final Map<String, Marker> _markers = {};
   List coordinates = [];
   int fullness;
+  int batteryPercent;
+  bool isAdmin = false;
 
   void setCustomMapPin() async {
-    pinLocationIcon =
-        await BitmapDescriptor.fromAssetImage(ImageConfiguration(devicePixelRatio: 2.5), 'assets/heroStationIcon.png');
+    pinLocationIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(devicePixelRatio: 2.5),
+        'assets/heroStationIcon.png');
   }
 
   int latitude;
@@ -42,11 +50,13 @@ class _MapViewState extends State<MapView> {
       coordinates.add(snapshot.value["3566"]["Location"]["latitude"]);
       coordinates.add(snapshot.value["3566"]["Location"]["longtitude"]);
       fullness = snapshot.value["3566"]["FULLNESS"];
+      batteryPercent = snapshot.value["3566"]["BATTERY_CAPACITY"];
     });
   }
 
   Future<void> _onMapCreated(GoogleMapController controller) async {
-    final Uint8List markerIcon = await getBytesFromAsset('assets/heroStationIcon.png', 100);
+    final Uint8List markerIcon =
+        await getBytesFromAsset('assets/heroStationIcon.png', 100);
     // final recycleBins = await locations.getHeroStations();
     await readData().whenComplete(() => setState(() {
           print(fullness.toString());
@@ -55,14 +65,38 @@ class _MapViewState extends State<MapView> {
             markerId: MarkerId("HeroStation"),
             icon: BitmapDescriptor.fromBytes(markerIcon),
             position: LatLng(coordinates[0], coordinates[1]),
-            infoWindow: InfoWindow(title: "HeroStation", snippet: "%" + fullness.toString() + " dolu"),
+            infoWindow: isAdmin
+                ? InfoWindow(
+                    title: "HeroStation",
+                    snippet:
+                        "% ${fullness.toString()} Dolu \n% {$batteryPercent} Şarj")
+                : InfoWindow(
+                    title: "HeroStation",
+                    snippet: "%" + fullness.toString() + " Dolu "),
           );
           _markers["HeroStation"] = marker;
         }));
   }
 
+  Future checkAdmin() async {
+    final currentUid = FirebaseAuth.instance.currentUser.uid;
+
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(currentUid)
+        .get()
+        .then((DocumentSnapshot<Map<String, dynamic>> value) {
+      if (value.data().containsKey('admin')) {
+        if (value.data()['admin'] == true) {
+          isAdmin = true;
+        }
+      }
+    });
+  }
+
   @override
   void initState() {
+    checkAdmin();
     super.initState();
     setCustomMapPin();
   }
@@ -86,7 +120,8 @@ class _MapViewState extends State<MapView> {
                 ),
               );
             },
-            child: Image.asset("assets/Icons/eventrequestwhite.png", scale: 1.1),
+            child:
+                Image.asset("assets/Icons/eventrequestwhite.png", scale: 1.1),
           ),
           SizedBox(
             width: size.width * 0.01,
